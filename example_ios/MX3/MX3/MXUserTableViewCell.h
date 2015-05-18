a72ce3e9d8d9ce29cbd5bd391058efd7cfa078d3
Joey Wessel <joey.wessel@gmail.com>
2015-05-18 19:02:59 -0400

    Added AFNetworking and some UI categories.

---
+++ example_ios/MX3/MX3/MXUserTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MXUserTableViewCell.h
+//  MX3
+//
+//  Created by Joey Wessel on 5/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXUserTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (weak, nonatomic) IBOutlet UIImageView *userAvatar;
+
+
+@end
---
+++ example_ios/MX3/MX3/MXUserTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MXUserTableViewCell.m
+//  MX3
+//
+//  Created by Joey Wessel on 5/15/15.
+//
+//
+
+#import "MXUserTableViewCell.h"
+
+@implementation MXUserTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
